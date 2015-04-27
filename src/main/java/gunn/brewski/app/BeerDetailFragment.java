d727d176fbde9f3a2174b614837cc092b6423824
Cliff Gunn <clifton.gunn@schneider-electric.com>
2015-04-26 22:35:06 -0500

    Got started working on the Detail views for each type. Only made it half way through the Beer Detail. Forrest Gump was on TV. It distracted me a bit.

--- src/main/java/gunn/brewski/app/BeerDetailFragment.java
+++ src/main/java/gunn/brewski/app/BeerDetailFragment.java
@@ -19,6 +19,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import gunn.brewski.app.data.BrewskiContract;
+import gunn.brewski.app.data.BrewskiContract.BeerEntry;
+
 /**
  * Created by SESA300553 on 4/7/2015.
  */
@@ -27,52 +30,45 @@ public class BeerDetailFragment extends Fragment implements LoaderManager.Loader
     private static final String LOG_TAG = BeerDetailFragment.class.getSimpleName();
     static final String BEER_DETAIL_URI = "BEER_URI";
 
-    private static final String FORECAST_SHARE_HASHTAG = " #BrewskiApp";
+    private static final String BEER_SHARE_HASHTAG = " #BrewskiBeer";
 
-    private ShareActionProvider mShareActionProvider;
-    private String mForecast;
-    private Uri mUri;
+    private ShareActionProvider mBeerShareActionProvider;
+    private String mBeer;
+    private Uri mBeerUri;
 
     private static final int BEER_DETAIL_LOADER = 0;
 
-    private static final String[] DETAIL_COLUMNS = {
-//            WeatherEntry.TABLE_NAME + "." + WeatherEntry._ID,
-//            WeatherEntry.COLUMN_DATE,
-//            WeatherEntry.COLUMN_SHORT_DESC,
-//            WeatherEntry.COLUMN_MAX_TEMP,
-//            WeatherEntry.COLUMN_MIN_TEMP,
-//            WeatherEntry.COLUMN_HUMIDITY,
-//            WeatherEntry.COLUMN_PRESSURE,
-//            WeatherEntry.COLUMN_WIND_SPEED,
-//            WeatherEntry.COLUMN_DEGREES,
-//            WeatherEntry.COLUMN_WEATHER_ID,
-//            // This works because the WeatherProvider returns location data joined with
-//            // weather data, even though they're stored in two different tables.
-//            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING
+    private static final String[] BEER_DETAIL_COLUMNS = {
+        BeerEntry.TABLE_NAME + "." + BeerEntry._ID,
+        BeerEntry.COLUMN_BEER_ID,
+        BeerEntry.COLUMN_BEER_NAME,
+        BeerEntry.COLUMN_BEER_DESCRIPTION,
+        BeerEntry.COLUMN_BREWERY_ID,
+        BeerEntry.COLUMN_CATEGORY_ID,
+        BeerEntry.COLUMN_STYLE_ID,
+        BeerEntry.COLUMN_LABEL_LARGE,
+        BeerEntry.COLUMN_LABEL_MEDIUM,
+        BeerEntry.COLUMN_LABEL_ICON,
     };
 
     // These indices are tied to DETAIL_COLUMNS.  If DETAIL_COLUMNS changes, these
     // must change.
-//    public static final int COL_WEATHER_ID = 0;
-//    public static final int COL_WEATHER_DATE = 1;
-//    public static final int COL_WEATHER_DESC = 2;
-//    public static final int COL_WEATHER_MAX_TEMP = 3;
-//    public static final int COL_WEATHER_MIN_TEMP = 4;
-//    public static final int COL_WEATHER_HUMIDITY = 5;
-//    public static final int COL_WEATHER_PRESSURE = 6;
-//    public static final int COL_WEATHER_WIND_SPEED = 7;
-//    public static final int COL_WEATHER_DEGREES = 8;
-//    public static final int COL_WEATHER_CONDITION_ID = 9;
+    public static final int COL_BEER_ID = 0;
+    public static final int COL_BEER_NAME = 1;
+    public static final int COL_BEER_DESCRIPTION = 2;
+    public static final int COL_BREWERY_ID = 3;
+    public static final int COL_CATEGORY_ID = 4;
+    public static final int COL_STYLE_ID = 5;
+    public static final int COL_LABEL_LARGE = 6;
+    public static final int COL_LABEL_MEDIUM = 7;
+    public static final int COL_LABEL_ICON = 8;
 
-//    private ImageView mIconView;
-//    private TextView mFriendlyDateView;
-//    private TextView mDateView;
-//    private TextView mDescriptionView;
-//    private TextView mHighTempView;
-//    private TextView mLowTempView;
-//    private TextView mHumidityView;
-//    private TextView mWindView;
-//    private TextView mPressureView;
+    private ImageView mBeerLabelIconView;
+    private TextView mBeerNameView;
+    private TextView mBeerDescriptionView;
+    private TextView mBreweryName;
+    private TextView mCategoryName;
+    private TextView mStyleName;
 
     public BeerDetailFragment() {
         setHasOptionsMenu(true);
@@ -84,47 +80,43 @@ public class BeerDetailFragment extends Fragment implements LoaderManager.Loader
 
         Bundle arguments = getArguments();
         if (arguments != null) {
-            mUri = arguments.getParcelable(BeerDetailFragment.BEER_DETAIL_URI);
+            mBeerUri = arguments.getParcelable(BeerDetailFragment.BEER_DETAIL_URI);
         }
 
-//        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-//        mIconView = (ImageView) rootView.findViewById(R.id.detail_icon);
-//        mDateView = (TextView) rootView.findViewById(R.id.detail_date_textview);
-//        mFriendlyDateView = (TextView) rootView.findViewById(R.id.detail_day_textview);
-//        mDescriptionView = (TextView) rootView.findViewById(R.id.detail_forecast_textview);
-//        mHighTempView = (TextView) rootView.findViewById(R.id.detail_high_textview);
-//        mLowTempView = (TextView) rootView.findViewById(R.id.detail_low_textview);
-//        mHumidityView = (TextView) rootView.findViewById(R.id.detail_humidity_textview);
-//        mWindView = (TextView) rootView.findViewById(R.id.detail_wind_textview);
-//        mPressureView = (TextView) rootView.findViewById(R.id.detail_pressure_textview);
-//        return rootView;
-        return null;
+        View rootView = inflater.inflate(R.layout.fragment_beer_detail, container, false);
+        mBeerLabelIconView = (ImageView) rootView.findViewById(R.id.detail_beer_icon);
+        mBeerNameView = (TextView) rootView.findViewById(R.id.detail_beer_name);
+        mBeerDescriptionView = (TextView) rootView.findViewById(R.id.detail_beer_description);
+        mBreweryName = (TextView) rootView.findViewById(R.id.detail_brewery_name);
+        mCategoryName = (TextView) rootView.findViewById(R.id.detail_category_name);
+        mStyleName = (TextView) rootView.findViewById(R.id.detail_style_name);
+        return rootView;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        inflater.inflate(R.menu.detailfragment, menu);
+        inflater.inflate(R.menu.menu_beer_detail, menu);
 
         // Retrieve the share menu item
-//        MenuItem menuItem = menu.findItem(R.id.action_share);
+        MenuItem menuItem = menu.findItem(R.id.action_beer_share);
 
         // Get the provider and hold onto it to set/change the share intent.
-//        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mBeerShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
         // If onLoadFinished happens before this, we can go ahead and set the share intent now.
-        if (mForecast != null) {
-//            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        if (mBeer != null) {
+            mBeerShareActionProvider.setShareIntent(createShareBeerIntent());
         }
     }
 
-//    private Intent createShareForecastIntent() {
-//        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-//        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-//        shareIntent.setType("text/plain");
-//        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecast + FORECAST_SHARE_HASHTAG);
-//        return shareIntent;
-//    }
+    private Intent createShareBeerIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mBeer + BEER_SHARE_HASHTAG);
+        return shareIntent;
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -132,26 +124,28 @@ public class BeerDetailFragment extends Fragment implements LoaderManager.Loader
         super.onActivityCreated(savedInstanceState);
     }
 
-    void onLocationChanged( String newLocation ) {
+    void onBeerChanged( String newLocation ) {
         // replace the uri, since the location has changed
-        Uri uri = mUri;
+        Uri uri = mBeerUri;
         if (null != uri) {
+            // TODO: THIS IS WHERE I NEED TO PULL IN THE BREWERY, CATEGORY, AND STYLE NAMES.
+
 //            long date = WeatherContract.WeatherEntry.getDateFromUri(uri);
 //            Uri updatedUri = WeatherContract.WeatherEntry.buildWeatherLocationWithDate(newLocation, date);
-//            mUri = updatedUri;
+//            mBeerUri = updatedUri;
             getLoaderManager().restartLoader(BEER_DETAIL_LOADER, null, this);
         }
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if ( null != mUri ) {
+        if ( null != mBeerUri ) {
             // Now create and return a CursorLoader that will take care of
             // creating a Cursor for the data being displayed.
             return new CursorLoader(
                     getActivity(),
-                    mUri,
-                    DETAIL_COLUMNS,
+                    mBeerUri,
+                    BEER_DETAIL_COLUMNS,
                     null,
                     null,
                     null
@@ -164,56 +158,53 @@ public class BeerDetailFragment extends Fragment implements LoaderManager.Loader
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         if (data != null && data.moveToFirst()) {
             // Read weather condition ID from cursor
-//            int weatherId = data.getInt(COL_WEATHER_CONDITION_ID);
-//
-//            // Use weather art image
-//            mIconView.setImageResource(Utility.getArtResourceForWeatherCondition(weatherId));
-//
-//            // Read date from cursor and update views for day of week and date
-//            long date = data.getLong(COL_WEATHER_DATE);
-//            String friendlyDateText = Utility.getDayName(getActivity(), date);
-//            String dateText = Utility.getFormattedMonthDay(getActivity(), date);
-//            mFriendlyDateView.setText(friendlyDateText);
-//            mDateView.setText(dateText);
-//
-//            // Read description from cursor and update view
-//            String description = data.getString(COL_WEATHER_DESC);
-//            mDescriptionView.setText(description);
-//
-//            // For accessibility, add a content description to the icon field
-//            mIconView.setContentDescription(description);
-//
-//            // Read high temperature from cursor and update view
-//            boolean isMetric = Utility.isMetric(getActivity());
-//
-//            double high = data.getDouble(COL_WEATHER_MAX_TEMP);
-//            String highString = Utility.formatTemperature(getActivity(), high);
-//            mHighTempView.setText(highString);
-//
-//            // Read low temperature from cursor and update view
-//            double low = data.getDouble(COL_WEATHER_MIN_TEMP);
-//            String lowString = Utility.formatTemperature(getActivity(), low);
-//            mLowTempView.setText(lowString);
-//
-//            // Read humidity from cursor and update view
-//            float humidity = data.getFloat(COL_WEATHER_HUMIDITY);
-//            mHumidityView.setText(getActivity().getString(R.string.format_humidity, humidity));
-//
-//            // Read wind speed and direction from cursor and update view
-//            float windSpeedStr = data.getFloat(COL_WEATHER_WIND_SPEED);
-//            float windDirStr = data.getFloat(COL_WEATHER_DEGREES);
-//            mWindView.setText(Utility.getFormattedWind(getActivity(), windSpeedStr, windDirStr));
-//
-//            // Read pressure from cursor and update view
-//            float pressure = data.getFloat(COL_WEATHER_PRESSURE);
-//            mPressureView.setText(getActivity().getString(R.string.format_pressure, pressure));
+            int beerId = data.getInt(COL_BEER_ID);
+
+            // Use weather art image
+            mBeerLabelIconView.setImageResource(Utility.getArtResourceForWeatherCondition(beerId));
+
+            // Read description from cursor and update view
+            String beerName = data.getString(COL_BEER_NAME);
+            mBeerNameView.setText(beerName);
+
+            // Read description from cursor and update view
+            String beerDescription = data.getString(COL_BEER_DESCRIPTION);
+            mBeerDescriptionView.setText(beerDescription);
+
+            // For accessibility, add a content description to the icon field
+            mBeerLabelIconView.setContentDescription(beerDescription);
+
+            // Read high temperature from cursor and update view
+            boolean isMetric = Utility.isMetric(getActivity());
+
+            double high = data.getDouble(COL_WEATHER_MAX_TEMP);
+            String highString = Utility.formatTemperature(getActivity(), high);
+            mHighTempView.setText(highString);
+
+            // Read low temperature from cursor and update view
+            double low = data.getDouble(COL_WEATHER_MIN_TEMP);
+            String lowString = Utility.formatTemperature(getActivity(), low);
+            mLowTempView.setText(lowString);
+
+            // Read humidity from cursor and update view
+            float humidity = data.getFloat(COL_WEATHER_HUMIDITY);
+            mHumidityView.setText(getActivity().getString(R.string.format_humidity, humidity));
+
+            // Read wind speed and direction from cursor and update view
+            float windSpeedStr = data.getFloat(COL_WEATHER_WIND_SPEED);
+            float windDirStr = data.getFloat(COL_WEATHER_DEGREES);
+            mWindView.setText(Utility.getFormattedWind(getActivity(), windSpeedStr, windDirStr));
+
+            // Read pressure from cursor and update view
+            float pressure = data.getFloat(COL_WEATHER_PRESSURE);
+            mPressureView.setText(getActivity().getString(R.string.format_pressure, pressure));
 
             // We still need this for the share intent
-//            mForecast = String.format("%s - %s - %s/%s", dateText, description, high, low);
+            mForecast = String.format("%s - %s - %s/%s", dateText, description, high, low);
 
             // If onCreateOptionsMenu has already happened, we need to update the share intent now.
-            if (mShareActionProvider != null) {
-//                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            if (mBeerShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
             }
         }
     }
--- src/main/java/gunn/brewski/app/CategoryListFragment.java
+++ src/main/java/gunn/brewski/app/CategoryListFragment.java
@@ -20,7 +20,6 @@ import android.widget.ListView;
 
 
 import gunn.brewski.app.data.BrewskiContract;
-import gunn.brewski.app.dummy.DummyContent;
 import gunn.brewski.app.sync.BrewskiSyncAdapter;
 
 /**
